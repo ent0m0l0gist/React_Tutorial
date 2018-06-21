50d05fa3ec21273e8814b06f645944e320217022
Raghav Agrawal <raghav151196@gmail.com>
2018-06-21 15:26:21 +0530

    after module 4 : list and condition

--- src/App.js
+++ src/App.js
@@ -3,47 +3,71 @@ import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person'
 
-const myFunc = () =>
-{
-	alert("HELLO");
-}
-
 class App extends Component {
    state = {
    		persons:[
-   		{name:"Raghav", age:21},
-   		{name:"Leo", age: 31}
-   		]
+   		{id:'asdf12',name:'Raghav', age:21},
+   		{id:'ghtr123',name:'Leo', age: 31}
+		],
+		showPersons:false
+		
    	}
 
-   	myFunc=(newName) =>
-    {
-	//alert("HELLO");
-	this.setState( {
-		persons:[
-   		{name:"Raghav Agrawal", age:21},
-   		//{name:"Leo", age: 312}
-   		{name: newName, age:50}
-   		]
-   				}
-		)
-	} 
+
+	deletePersonHandler=(deleteIndex)=>
+	{
+		//first make a copy of state, make changes to that copy, and then update the state.
+		//const person = this.state.persons just copies the pointer. any change to persons will be made to the state as well immediately.
+		const persons = [...this.state.persons];
+		persons.splice(deleteIndex,1);
+		this.setState({persons:persons});
+
+		//passing this index helps us to work on particular element of list
+	}
+
+	nameChangeHandler= (event,id) => {
+	
+		const personIndex = this.state.persons.findIndex(p=>{
+			return p.id === id;
+		});
+
+		const person = {...this.state.persons[personIndex]};
+		person.name = event.target.value;
+
+		const newPersons = [...this.state.persons];
+		newPersons[personIndex]=person;
+		//alert(person.name);
+		//console.log(person.name);
+		this.setState({persons:newPersons});
+	}	
 	
 
-	nameChangeHandler=(event)=>
-	{
-		this.setState(
-			{
-				persons:[
-					{name: event.target.value, age:21},
-					//{name:"Leo", age: 312}
-					{name: "Messi", age:50}
-					]
-			}
-		)
+	togglePersons=()=>{
+		var temp = this.state.showPersons;
+		this.setState({showPersons: !temp});
 	}
 
    render() {  
+//render function is called again when react reloads a component
+
+	let persons = null;
+
+	
+	if ( this.state.showPersons ) {
+		persons = (
+		  <div>
+			{this.state.persons.map((person, index) => {
+			  return <Person
+				click={() => this.deletePersonHandler(index)}
+				name={person.name} 
+				age={person.age}
+				key={person.id}
+				changed={(event) => this.nameChangeHandler(event, person.id)} />
+			})}
+		  </div>
+		);
+	  }
+
 	   
 	const style = {
 		backgroundColor : 'white',
@@ -54,14 +78,16 @@ class App extends Component {
 
       <div className="App">
        		<h1>Hi, I am a React app </h1>
-
-       		<button style = {style} onClick={()=>this.myFunc("MEssi messi")}>Switch Name</button>
-       		<Person name ={this.state.persons[0].name} age = {this.state.persons[0].age} changed={this.nameChangeHandler}>huhahuhahuha</Person>
-       		<Person name ={this.state.persons[1].name} age = {this.state.persons[1].age} click={this.myFunc.bind(this, "lipistic")}/>
-       		
+       		<button style = {style} onClick={this.togglePersons}>Toggle</button>
+			{persons}
+			
       </div>
     );
   }
+  //ONE WAY TO PUT CONDITION : this.state.showPersons ? (JSX HERE) : (else wala JSX)
+
+
+  //<button style = {style} onClick={()=>this.myFunc("MEssi messi")}>Switch Name</button>
   //different ways to bind values : 1. arrow function  2. using this.func.bind()
   ///JSX gets con erted to the thing below
   //return React.createElement('div',{className:'App'},React.createElement('h1',null,'Through react createElement'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,7 +6,7 @@ const Person = (props) =>
 		<div className="Person">
 		<p onClick={props.click}>I am {props.name} and my age is {props.age} </p>
 		<p>{props.children}</p>
-		<input onChange={props.changed} value={props.name}/>
+		<input onChange={props.changed} value={props.name} />
 		</div>
 		)
 }
